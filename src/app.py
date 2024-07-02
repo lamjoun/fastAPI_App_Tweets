9c0cd2da350c41c0bade600198e6995115c9d999
Rachid Lamjoun <rlamjoun@gmail.com>
2024-07-02 22:44:50 +0200

    Update .yml file for github Action

--- src/app.py
+++ src/app.py
@@ -140,5 +140,5 @@ async def prediction(input_parameters: Tweet):
 
 
 # Démarrer le serveur
-#if __name__ == "__main__":
-#    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
